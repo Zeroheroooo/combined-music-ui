c6ce1341e0b62856662721847a8125d9737b4073
Zeroheroooo <147383657+Zeroheroooo@users.noreply.github.com>
2026-04-21 00:00:36 +0800

    deal with bomb missing

--- app.js
+++ app.js
@@ -402,7 +402,7 @@ class Bomb {
 // 【音樂對拍系統：為炸彈加入完美掉落時間參數】 + targetTime
   constructor(x, y, targetTime, spawnTime) { 
     this.x = x ?? Math.random() * (WIDTH - Bomb.WIDTH);
-    this.y = y ?? -Bomb.HEIGHT;
+    this.startY = y ?? -Bomb.HEIGHT;
     this.y = this.startY;//0420 
     // ****************************************************************************
     // ****************************************************************************
